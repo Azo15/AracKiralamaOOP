c0b781ec72715802e6567fc75922381c8a01016b
Azo Ismail <ismailazo260@gmail.com>
2026-01-01 19:30:45 +0300

    feat: Add initial Main class to demonstrate vehicle rental system functionality.

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        // 1. Once sirketi kurduk
+        //Once sirketi kurduk
         KiralamaSirketi sirket = new KiralamaSirketi();
 
-        // 2. Ornek araclari (nesneleri) olusturduk
+        //Ornek araclari (nesneleri) olusturduk
         Otomobil oto1 = new Otomobil("Audi", 2026, 1200, 4);
         Kamyon kamyon1 = new Kamyon("Volvo", 2020, 2500, 25);
 
@@ -16,7 +16,7 @@ public class Main {
         Otomobil oto3 = new Otomobil("Ford", 2022, 1200, 4);
         Kamyon kamyon3 = new Kamyon("Fiat", 2023, 2500, 20);
 
-        // 3. Araclari galeriye ekledik (Polimorfizm)
+        //Araclari galeriye ekledik (Polimorfizm)
         sirket.aracEkle(oto1);
         sirket.aracEkle(kamyon1);
         sirket.aracEkle(oto2);
@@ -24,10 +24,10 @@ public class Main {
         sirket.aracEkle(oto3);
         sirket.aracEkle(kamyon3);
 
-        // 4. Hepsini listeledik
+        //Hepsini listeledik
         sirket.araclariListele();
 
-        // 5. Kiralama ve bakim testi
+        //Kiralama ve bakim testi
         sirket.aracKirala(oto1);
         sirket.aracKirala(kamyon1);
 
